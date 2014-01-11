2c2ac7b1c220cb9e23c6b7290cb389e5f1930daa
Matthew Spellings <mspells@umich.edu>
2014-01-10 21:16:19 -0500

    Moved to using parsec

---
+++ src/Text/Temper.hs
@@ -0,0 +1,122 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Text.Temper where
+
+import Control.Applicative ((<$>))
+import Control.Monad (when)
+import qualified Data.Map as M
+import Data.Maybe
+import Data.Text as T
+import Text.Parsec
+import Text.Parsec.Char
+import Text.Parsec.Text
+
+data Element = Tag {tagName::Text,
+                    ident::Maybe Text,
+                    classes::[Text],
+                    attributes::M.Map Text Text,
+                    children::[Element]} |
+               TextNode {nodeText::Text} |
+               SpecialTag {specialTagContents::Text} deriving Show
+
+emptyTag::Element
+emptyTag = Tag "" Nothing [] M.empty []
+
+tagModifier::Parser (Element->Element)
+tagModifier = do
+  c <- oneOf ['.', '#']
+  n <- many1 . noneOf $ [' ', '.', '#', '\r', '\n']
+  case c of
+    '.' -> return (\elt -> elt {classes=(T.pack n:classes elt)})
+    '#' -> return (\elt -> elt {ident=Just . pack $ n})
+    _ -> unexpected "Unhandled tag modifier case"
+
+tagStart::Parser Element
+tagStart = do
+  tname <- many1 . noneOf $ [' ', '.', '#', '\r', '\n']
+  mods <- many tagModifier
+  return (Prelude.foldr (.) id mods $ emptyTag {tagName=T.pack tname})
+
+quote = oneOf ['"', '\'']
+
+onlySpace = oneOf [' ', '\t']
+
+tagAttribute::Parser (Text, Text)
+tagAttribute = do
+  name <- many1 . noneOf $ ['=', ' ', '$']
+  _ <- char '='
+  value <- between quote quote (many . noneOf $ ['"', '\''])
+  return (T.pack name, T.pack value)
+
+tagAttributes::Parser (M.Map Text Text)
+tagAttributes = do
+  pairs <- try tagAttribute `sepEndBy` many1 onlySpace
+  return (M.fromList pairs)
+
+tag::Int->Parser Element
+tag minimumIndent = do
+  _ <- Text.Parsec.count minimumIndent onlySpace
+  addIndent <- Prelude.length <$> many onlySpace
+  initialTag <- tagStart
+  _ <- many onlySpace
+  attributes <- tagAttributes
+  textChild <- optionMaybe (many onlySpace >> textNode 0)
+  _ <- many (try (many onlySpace >> newline))
+  children'' <- elements' (minimumIndent + addIndent + 1)
+  let children' = case textChild of
+        Just c -> c:children''
+        _ -> children''
+  return (initialTag {classes=Prelude.reverse . classes $ initialTag, attributes=attributes, children=children'})
+
+closedTag::Int->Parser Element
+closedTag minimumIndent = do
+  _ <- Text.Parsec.count minimumIndent onlySpace
+  addIndent <- Prelude.length <$> many onlySpace
+  initialTag <- tagStart
+  _ <- many onlySpace
+  attributes <- tagAttributes
+  _ <- many onlySpace >> char '/' >> many (try (many onlySpace >> newline))
+  return (initialTag {classes=Prelude.reverse . classes $ initialTag, attributes=attributes, children=[]})
+
+textNode::Int->Parser Element
+textNode minimumIndent = do
+  _ <- Text.Parsec.count minimumIndent onlySpace
+  addIndent <- Prelude.length <$> many onlySpace
+  _ <- char '$' >> many onlySpace
+  rest <- many . noneOf $ ['\r', '\n']
+  _ <- many (try (many onlySpace >> newline))
+  return (TextNode . T.pack $ rest)
+
+specialTag::Parser Element
+specialTag = do
+  _ <- char '!'
+  rest <- many . noneOf $ ['\r', '\n']
+  _ <- many (try (many onlySpace >> newline))
+  return (SpecialTag . T.pack $ rest)
+
+element::Int->Parser Element
+element minimumIndent = try specialTag <|> try (textNode minimumIndent) <|> try (closedTag minimumIndent) <|> try (tag minimumIndent)
+
+elements'::Int->Parser [Element]
+elements' minimumIndent = many $ element minimumIndent
+
+elements::Parser [Element]
+elements = elements' 0
+
+encodeElement::Element->Text
+encodeElement (Tag name ident classes attributes children) =
+  T.concat ['<' `T.cons` name, ident', classes', attributes', rest]
+  where
+    rest
+      | Prelude.null children = " />"
+      | otherwise =  '>' `T.cons` children' `T.append` closure
+    ident' = maybe T.empty (\x -> " id=\"" `T.append` x `T.snoc` '"') ident
+    classes'
+      | Prelude.null classes = T.empty
+      | otherwise = " class=\"" `T.append` T.unwords classes `T.snoc` '"'
+    attributes' = T.concat $ T.cons ' ' . translateAttr <$> M.toList attributes
+    translateAttr (name, val) = name `T.append` "=\"" `T.append` val `T.snoc` '"'
+    children' = T.concat $ encodeElement <$> children
+    closure = "</" `T.append` name `T.snoc` '>'
+encodeElement (TextNode conts) = conts
+encodeElement (SpecialTag conts) = "<!" `T.append` conts `T.snoc` '>'
--- src/temper.hs
+++ src/temper.hs
@@ -6,22 +6,16 @@ import Data.Char (isSpace)
 import qualified Data.Map.Strict as M
 import Data.Monoid (mconcat)
 import Data.Ord (Ordering(..), compare)
-import Data.Text.Lazy (Text(..))
-import qualified Data.Text.Lazy as T
-import qualified Data.Text.Lazy.IO as IT (readFile, putStrLn)
+import Data.Text (Text(..))
+import qualified Data.Text as T
+import qualified Data.Text.IO as IT (readFile, putStrLn)
+import qualified Data.Text.Internal as LTI
 import Data.Text.Lazy.Builder (Builder(..), fromLazyText, toLazyText)
 import System.Console.CmdArgs
 import System.FilePath.Posix(joinPath)
 import System.IO (FilePath(..))
+import Text.Parsec.Prim (parse)
 import Text.Temper
-type ReplaceTable = M.Map Char T.Text
-
-baseTable::ReplaceTable
-baseTable = M.fromList [('"', "&quot;"),
-                        ('\'', "&apos;"),
-                        ('&', "&amp;"),
-                        ('<', "&lt;"),
-                        ('>', "&gt;")]
 
 data Options = Options {inFile :: String,
                         outFile :: String} deriving (Data, Typeable, Show)
@@ -29,113 +23,16 @@ data Options = Options {inFile :: String,
 defopts = Options {inFile = def &= typFile,
                    outFile = def}
 
-encodeHtml = T.concatMap (\c -> M.findWithDefault (T.singleton c) c baseTable)
-
-type Parser a = State (M.Map Int Text) a
-
-emptyParser = M.empty
-
-closeTop::Parser Text
-closeTop = do
-  tags <- get
-
-  case M.null tags of
-    False -> do
-      let ((_, top), rest) = M.deleteFindMax tags
-      put rest
-      return ('<' `T.cons` '/' `T.cons` top `T.snoc` '>')
-    _ -> return T.empty
-
-pushStack::Int->Text->Parser ()
-pushStack indent new = modify (M.insert indent new)
-
-expandAttributes::Text->Text
-expandAttributes name = T.concat [elt, ids, classes]
-  where
-    (elt:segments') = concat $ T.splitOn "#" <$> T.splitOn "." name
-    controllers = T.unpack $ T.filter (\c -> c == '#' || c == '.') name
-    segments = zip controllers segments'
-    ids' = filter ((== '#') . fst) segments
-    ids
-      | null ids' = T.empty
-      | otherwise = " id=\"" `T.append` (snd . head $ ids') `T.snoc` '"'
-    classes' = filter ((== '.') . fst) segments
-    classes
-      | null classes' = T.empty
-      | otherwise = " class=\"" `T.append` (T.unwords $ snd <$> classes') `T.snoc` '"'
-
-makeTag::Text->Parser Text
-makeTag x = do
-  let closeImmediately = (T.last x == '/') || (T.head x == '!')
-      (eltName:rest) = T.words x
-      eltName' = expandAttributes eltName
-
-  when closeImmediately $ do
-    _ <- closeTop
-    return ()
-  return ('<' `T.cons` eltName' `T.append` T.unwords rest `T.snoc` '>')
-
-cleanTag::Text->Text
-cleanTag = stripShortcuts . head . T.words
-  where
-    stripShortcuts = T.takeWhile (\c -> c /= '.' && c /= '#')
-
-indentationLevel::Text->Int
-indentationLevel = fromIntegral . T.length . T.takeWhile isSpace
-
-increaseIndentation::Int->Text->Parser Text
-increaseIndentation indent new = do
-  pushStack indent (cleanTag new)
-  makeTag new
-
-sameIndentation::Int->Text->Parser Text
-sameIndentation indent new = do
-  top <- closeTop
-  pushStack indent new
-  return top
-
-decreaseIndentation::Int->Text->Parser Text
-decreaseIndentation indent new = do
-  tags <- get
-  let (toClose, remainder) = M.partitionWithKey (\ind _ -> ind > indent) tags
-  put remainder
-  closed <- mconcat <$> mapM (\_ -> closeTop) (M.toDescList toClose)
-  return closed
-
-cleanParser::Parser Text
-cleanParser = do
-  tags <- get
-  closed <- mconcat <$> mapM (\_ -> closeTop) (M.toDescList tags)
-  put M.empty
-  return closed
-
-parseLine::Text->Parser Text
-parseLine line = do
-  tags <- get
-  let lastIndent = case M.null tags of
-        False -> fst . M.findMax $ tags
-        _ -> 0
-      indent = indentationLevel line
-      line' = T.strip line
-
-  case (compare indent lastIndent) of
-    LT -> decreaseIndentation indent line'
-    EQ -> sameIndentation indent line'
-    GT -> increaseIndentation indent line'
-
-parseLines::[Text]->Text
-parseLines ts = mconcat starts `T.append` rest
-  where
-    (starts, st) = runState (mapM parseLine ts) emptyParser
-    rest = evalState cleanParser st
-
-parseFile::Text->Text
-parseFile = parseLines . T.lines . encodeHtml
-
 main = do
   (Options inFile outFile) <- cmdArgs defopts
 
   cs <- IT.readFile inFile
-  IT.putStrLn $ parseFile cs
+
+  let result = parse elements "" cs
+      result' = case result of
+        Left err -> T.pack . show $ err
+        Right ts -> T.concat $ encodeElement <$> ts
+
+  IT.putStrLn result'
 
   return ()
